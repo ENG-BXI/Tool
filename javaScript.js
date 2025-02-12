b9589fc1cb8e9b7bb7bb3cb0c45aff48c3952c51
ENG_BXI <eng.0.bxi@gmail.com>
2025-02-12 22:11:13 +0300

    add some tool in js file

---
+++ javaScript.js
@@ -0,0 +1,7 @@
+// ScrollReveal
+//     to add animation when user scroll
+//     im used in pizza website
+// ============================================
+// swiper
+//     to add scroll item or slide
+//     im used in pizza website in pizza slider
